741fd1c624ee7ed62ebc285efc2c6372c451a40d
Mauro-AGalvezT <antoniogalte@outlook.com>
2021-03-28 11:17:20 -0500

    Creando Viewmodel

---
+++ Amiibopedia/Amiibopedia/Helpers/HttpsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Amiibopedia.Helpers
+{
+    public class HttpsHelper<T>
+    {
+        public async Task<T> GetRestServiceDataAsync(string serviceAddress)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(serviceAddress);
+            var response = await client.GetAsync(client.BaseAddress);
+            response.EnsureSuccessStatusCode();
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(jsonResult);
+            return result;
+        }
+    }
+}
---
+++ Amiibopedia/Amiibopedia/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Amiibopedia.Helpers;
+using Amiibopedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amiibopedia.ViewModels
+{
+    public class MainPageViewModel :BaseViewModel
+    {
+        public ObservableCollection<Character> Characters { get; set; }
+
+        public async Task LoadCharacters()
+        {
+            var url = "https://amiiboapi.com/api/character/";
+            var service = new HttpsHelper<Characters>();
+            var characters = await service.GetRestServiceDataAsync(url);
+            Characters = new ObservableCollection<Character>(characters.amiibo);
+
+        }
+
+    }
+}
